e62ffe9573771e3738abdb993ee400b0ad650065
dooddwebsite2 <dooddwebsite2@gmail.com>
2019-10-05 22:55:30 +0700

    facebook.js changed comment

--- facebooks.js
+++ facebooks.js
@@ -8,3 +8,4 @@
 //     js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
 //     fjs.parentNode.insertBefore(js, fjs);
 //     }(document, 'script', 'facebook-jssdk'));
+ 
