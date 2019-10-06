031679f35fbd73e2f80709b606bd98ec2638460e
dooddwebsite2 <dooddwebsite2@gmail.com>
2019-10-06 11:30:52 +0700

    add appid and fixed ie bug on chatbot

--- scripts.js
+++ scripts.js
@@ -1,4 +1,24 @@
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target) {
+    'use strict';
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
 
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
 /*
     window.fbAsyncInit = function() {
     FB.init({
@@ -20,19 +40,21 @@
 
 
 
-window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v4.0'
-    });
-  };
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '252960368604914',
+            autoLogAppEvents : true,
+            xfbml            : true,
+            version          : 'v4.0'
+          });
+        };
 
-  (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
 
 
